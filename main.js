98c8677b13b705e98c5aaaa1af0019edb2d9dea5
bee4rt <info.visuel@gmail.com>
2023-10-02 18:53:45 +0300

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 const title = document.querySelector(".title");
+const input = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+
+input.addEventListener("click", () => {
+  if (input.checked) {
+    title.textContent = "click to back to light mode";
+    title.style.color = "white";
+    bodyEl.style.backgroundColor = "black";
+  } else {
+    title.textContent = "click to change to dark mode";
+    title.style.color = "black";
+    bodyEl.style.backgroundColor = "white";
+  }
+});
+
+// function updateLocalStorage() {
+//   localStorage.setItem("mode", JSON.stringify(input.checked));
+// }
